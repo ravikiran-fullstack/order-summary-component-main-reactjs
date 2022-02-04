8553f91987102ddb4ad947aa8c8beaa5fd86aa2c
ravikiran <ravikiransjce.jobs@gmail.com>
2022-02-04 19:51:31 +0530

    Finished deskop and mobile version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='card'>
+        <div className='cardImage'></div>
+        <div className='cardBody'>
+          <div className='cardTitle'>
+            <h2>Order Summary</h2>
+          </div>
+          <div className='cardInfo'>
+            You can now listen to millions of songs, audiobooks and podcasts on
+            any device anywhere you like!
+          </div>
+          <div className='plan'>
+            <div className='planDetails'>
+              <div className='music-icon'></div>
+              <div>
+                <div>Annual Plan</div>
+                <p>$59.99/year</p>
+              </div>
+            </div>
+            <div className='planDetailsChange'>
+              <a href='#'>Change</a>
+            </div>
+          </div>
+          <button className='paymentBtn'>Proceed to Payment</button>
+          <button className='cancelBtn'>Cancel Order</button>
+        </div>
+      </div>
     </div>
   );
 }
